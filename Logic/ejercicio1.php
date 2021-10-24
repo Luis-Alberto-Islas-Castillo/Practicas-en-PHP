8211379094c84940460f1a359becb85861296563
Luis Alberto <luisalbertoislascastillo@gmail.com>
2021-10-24 18:39:36 -0500

    Practicando Logica
    Practicando mi Logica y conocimientos en PHP

---
+++ Logic/ejercicio1.php
@@ -0,0 +1,23 @@
+<?php
+
+/* Concatena dos cadenas con el operador punto (.) e imprimir su resultado*/
+
+$texto1='Holo ';
+$texto2='desde PHP :)';
+
+echo $texto1.$texto2;
+echo "\n";
+
+/*Hacer un programa que sume dos variables que almacenan dos números distintos*/
+
+$valor1=40;
+$valor2=46;
+
+echo "El resultado de la Suma es ".$valor1+$valor2;
+echo "\n";
+
+/* Mostrar en pantalla la tabla del 4  */
+for($i=4; $i<=40; $i=$i+4){                
+    echo "<br>";
+    echo $i;
+}
